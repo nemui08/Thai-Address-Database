03f4dafb6dcdbab8701069182b87f99b8bc38b1a
Napat1203 <6710301034@cdti.ac.th>
2024-12-18 22:59:46 +0700

    mainM & mainA

--- Thai Address Database-20241128/mainA.py
+++ Thai Address Database-20241128/mainA.py
@@ -6,94 +6,74 @@ sys.setrecursionlimit(10**6)
 
 
 with open('dataA.txt', 'r', encoding='utf-8') as fin:
-    AllRecord = []
+    all_rec = []
 
-    for Record in fin:
-       
-        Record = Record.strip()
-        if not Record:
+    for rec in fin:
+        rec = rec.strip()
+
+        if not rec:
             continue
 
-        Data = Record.split()
-        NewRecord = ' '.join(Data)
-        AllRecord.append(NewRecord)
-        
+        data = rec.split()
+        new_record = ' '.join(data)
+        all_rec.append(new_record)
+
 if __name__ == '__main__':
-    Bubble = list(AllRecord) 
-    Insert = list(AllRecord)
-    Select = list(AllRecord)
-    Merge = list(AllRecord)
-    Quick = list(AllRecord)
-    
-    start_b = time.time()    
-    md.bubblesort(Bubble)
-    end_b = time.time()
-    b = (end_b-start_b) * 10**3
-    print("bubblesort total time taken is ", b ,"milliseconds")
-    time_bubble = b
-    print()
-    
-    start_i = time.time()    
-    md.InsertionSort(Insert)
-    end_i = time.time()
-    i = (end_i-start_i) * 10**3
-    print("insertionSort total time taken is ", i ,"milliseconds")
-    time_insert = i
-    print()
-    
-    start_s = time.time()    
-    md.SelectionSort(Select)
-    end_s = time.time()
-    s = (end_s-start_s) * 10**3
-    print("SelectionSort total time taken is ", s ,"milliseconds")
-    time_select = s
-    print()
-    
-    start_m = time.time()    
-    md.MergeSort(Merge)
-    end_m = time.time()
-    m = (end_m-start_m) * 10**3
-    print("MergeSort total time taken is ", m ,"milliseconds")
-    time_merge = m
-    print()
-    
-    start_q = time.time()    
-    md.QuickSort(Quick,0 ,len(Quick)-1)
-    end_q = time.time()
-    q = (end_q-start_q) * 10**3
-    print("QuickSort total time taken is ", q ,"milliseconds")
-    time_quick = q
-    print()
-    
-    TotalTimes = [time_bubble, time_insert, time_select, time_merge, time_quick]
 
-    md.bubblesortLOW(TotalTimes)
-    
-    print(TotalTimes[0])
-    a = input("Please enter the algorithm name. You can check it from above. Enter , before entering the name = ")
-    TotalTimes[0] = f"{TotalTimes[0]}{a}"
+    algorithm_names = ["BubbleSort", "InsertionSort", "SelectionSort", "MergeSort", "QuickSort"]
+    bu, In, se, me, qu = all_rec, all_rec, all_rec, all_rec, all_rec
 
-    print(TotalTimes[1])
-    b = input("Please enter the algorithm name. You can check it from above. Enter , before entering the name = ")
-    TotalTimes[1] = f"{TotalTimes[1]}{b}"
+    # Bubble Sort
+    startB = time.time()
+    md.bubblesort(bu)
+    endB = time.time()
+    b = (endB - startB) * 10**3
+    print(f"{algorithm_names[0]} Total time taken {b:.2f} ms")
 
-    print(TotalTimes[2])
-    c = input("Please enter the algorithm name. You can check it from above. Enter , before entering the name = ")
-    TotalTimes[2] = f"{TotalTimes[2]}{c}"
+    # Insertion Sort
+    startI = time.time()
+    md.InsertionSort(In)
+    endI = time.time()
+    i = (endI - startI) * 10**3
+    print(f"{algorithm_names[1]} Total time taken {i:.2f} ms")
 
-    print(TotalTimes[3])
-    d = input("Please enter the algorithm name. You can check it from above. Enter , before entering the name = ")
-    TotalTimes[3] = f"{TotalTimes[3]}{d}"
+    # Selection Sort
+    startS = time.time()
+    md.SelectionSort(se)
+    endS = time.time()
+    s = (endS - startS) * 10**3
+    print(f"{algorithm_names[2]} Total time taken {s:.2f} ms")
 
-    print(TotalTimes[4])
-    e = input("Please enter the algorithm name. You can check it from above. Enter , before entering the name = ")
-    TotalTimes[4] = f"{TotalTimes[4]}{e}"
-    
-    print("Sort the time the Algorithm takes to do a task from most to least.", TotalTimes)
-    print()
-    print("Takes the most time is ", TotalTimes[-1])
-    print()
-    print("Takes the least amount of time is ", TotalTimes[0])
-    print()
-    print("Take a medium time is ", TotalTimes[2])
-    
+    # Merge Sort
+    startM = time.time()
+    md.MergeSort(me)
+    endM = time.time()
+    m = (endM - startM) * 10**3
+    print(f"{algorithm_names[3]} Total time taken {m:.2f} ms")
+
+    # Quick Sort
+    startQ = time.time()
+    md.QuickSort(qu, 0, len(qu) - 1)
+    endQ = time.time()
+    q = (endQ - startQ) * 10**3
+    print(f"{algorithm_names[4]} Total time taken {q:.2f} ms")
+
+    # Store time with algorithm name
+    total_time = [
+        (b, algorithm_names[0]),
+        (i, algorithm_names[1]),
+        (s, algorithm_names[2]),
+        (m, algorithm_names[3]),
+        (q, algorithm_names[4])
+    ]
+
+    # Sort from most to least
+    total_time.sort(key=lambda x: x[0], reverse=True)
+
+    print("\nArrange the time the algorithm takes to do from most to least:")
+    for t in total_time:
+        print(f"{t[1]}: {t[0]:.2f} ms")
+
+    print(f"\nTakes the most time: {total_time[0][1]} ({total_time[0][0]:.2f} ms)")
+    print(f"Take a medium time: {total_time[len(total_time)//2][1]} ({total_time[len(total_time)//2][0]:.2f} ms)")
+    print(f"Takes the least time: {total_time[-1][1]} ({total_time[-1][0]:.2f} ms)")
