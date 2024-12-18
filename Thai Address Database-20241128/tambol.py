17631613a257c8b7f54843248fae3ab743cbfe60
Natee <natee33067@gmail.com>
2024-12-18 21:37:20 +0700

    Province

---
+++ Thai Address Database-20241128/tambol.py
@@ -0,0 +1,25 @@
+import re
+
+input_file = 'tambol.sql'  
+output_file = 'dataT.txt' 
+
+def extract_data(file_path):
+    data = []
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            
+            match = re.search(r"INSERT INTO\s+`tambol`\s*\(.*?\)\s*VALUES\s*\(\s*(\d+),\s*'([^']+)'", line)
+            if match:
+                code, name = match.groups()
+                data.append(f'{code}, {name}')
+                
+    return data
+
+def out_data(data, file_path):
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write('\n'.join(data))
+        
+
+a = extract_data(input_file)
+b = out_data(a, output_file)
